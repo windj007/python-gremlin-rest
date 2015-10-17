12a9f8eca799aac0e319720e89c96ef82ddff3c3
windj007 <windj007@gmail.com>
2015-10-17 15:58:19 +0300

    fix script (do not rely on exception handling)

--- gremlin_rest/scripts/upsert_vertex_custom_id.groovy
+++ gremlin_rest/scripts/upsert_vertex_custom_id.groovy
@@ -1,23 +1,21 @@
 def v = null;
 def created = false;
-try {
-	if (label != null)
-		v = graph.traversal().V().has(T.label, label).has(id_prop, id_value).next();
-	else
-		v = graph.traversal().query().has(id_prop, id_value).next();
-catch (NoSuchElementException) {
-}
-if (v == null) {
+def v_iter = (label != null) ?
+	  graph.traversal().V().has(T.label, label).has(id_prop, id_value)
+	: graph.traversal().query().has(id_prop, id_value);
+if (v_iter.hasNext())
+	v = v_iter.next();
+else {
 	if (label != null)
 		v = graph.addVertex(T.label, label);
 	else
 		v = graph.addVertex();
 	v.property(id_prop, id_value);
 	created = true;
-};
+}
 properties.each { k, val ->
 	v.property(k, val);
-};
+}
 graph.tx().commit();
 
 [v, created]
